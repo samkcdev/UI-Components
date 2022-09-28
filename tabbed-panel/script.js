a1b54d3d3142aa76426248a5d68356958a466fd4
samkcdev <samnp87@gmail.com>
2022-09-28 16:17:59 +0530

    done with tab panel functionality

--- tabbed-panel/script.js
+++ tabbed-panel/script.js
@@ -1,29 +1,40 @@
 let tabs = document.querySelectorAll(".tabs li a");
-
+let panels = document.querySelectorAll(".panel-content");
 console.log(tabs);
 
 let activetab = () => {
-  for (tab of tabs) {
+  for (let tab of tabs) {
     if (tab.classList.contains("active-tab")) {
       return tab;
     }
   }
 };
 
+let activePanel = () => {
+  for (let panel of panels) {
+    if (panel.classList.contains("open")) {
+      return panel;
+    }
+  }
+};
+
 function operatePanels(e) {
   //find which tab i have clicked on
-  let clickedTab = e.srcElement;
-  let href = clickedTab.href;
-  let activeTab = activetab();
-  console.log(clickedTab.hash);
 
+  let clickedTab = e.srcElement;
+  let activeTab = activetab();
+  let currentactivePanel = activePanel();
+
+  panels.forEach((panel) => {
+    if (clickedTab.hash.replace(/[#]/gi, "") === panel.id) {
+      panel.classList.add("open");
+      currentactivePanel.classList.remove("open");
+    }
+  });
+
+  //* logic for the tabs
   activeTab.classList.remove("active-tab");
   clickedTab.classList.add("active-tab");
-
-  console.log(href);
-
-  //get the classname if open then remove and add closed else reverse
-  //also get the tab which has open class (find active tab) remove that class and add closed
 }
 
 tabs.forEach((tab) => {
