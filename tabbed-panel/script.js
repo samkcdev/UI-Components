6b1c6ddf4cabc2fea19a873ef7e1dc0a5e2433b8
samkcdev <samnp87@gmail.com>
2022-09-28 20:40:16 +0530

    completed with bugfix

--- tabbed-panel/script.js
+++ tabbed-panel/script.js
@@ -1,40 +1,26 @@
 let tabs = document.querySelectorAll(".tabs li a");
 let panels = document.querySelectorAll(".panel-content");
-console.log(tabs);
-
-let activetab = () => {
-  for (let tab of tabs) {
-    if (tab.classList.contains("active-tab")) {
-      return tab;
-    }
-  }
-};
-
-let activePanel = () => {
-  for (let panel of panels) {
-    if (panel.classList.contains("open")) {
-      return panel;
-    }
-  }
-};
 
 function operatePanels(e) {
+  console.log(panels);
   //find which tab i have clicked on
-  console.log(e);
   let selectedTab = e.target;
-  let currentTab = activetab();
-  let currentPanel = activePanel();
 
-  panels.forEach((panel) => {
-    if (selectedTab.hash.replace(/[#]/gi, "") === panel.id) {
-      panel.classList.add("open");
-      currentPanel.classList.remove("open");
-    }
-  });
+  document
+    .querySelector(".tabs li a.active-tab")
+    .classList.remove("active-tab");
 
-  //* logic for the tabs
-  currentTab.classList.remove("active-tab");
+  document.querySelector(".open").classList.remove("open");
+
   selectedTab.classList.add("active-tab");
+
+  const panel = [...panels].filter(
+    (ele) => ele.getAttribute("id") === selectedTab.hash.replace("#", "")
+  );
+
+  console.log(panel);
+
+  panel[0].classList.add("open");
 }
 
 tabs.forEach((tab) => {
